17b6bd5504bcbd33af7f0e4c618b391f188f9bb1
Mark Lamourine <markllama@gmail.com>
2025-02-23 11:32:33 -0500

    Add docs and remove unneeded steps

--- scripts/buildah-fedora-dhcpd.sh
+++ scripts/buildah-fedora-dhcpd.sh
@@ -1,38 +1,36 @@
 #!/bin/bash
-#
+# -------------------------------------
 # Build a container for dhcpd on Fedora
-#
+# -------------------------------------
+
+# To tag and publish the image
+# buildah tag localhost/dhcpd-fedora quay.io/markllama/dhcpd-fedora
+# buildah push quay.io/markllama/dhcpd-fedora
+
+# Stop on any error 
 set -o errexit
 
 function main() {
     # Create a container
     CONTAINER=$(buildah from registry.fedoraproject.org/fedora-minimal:41)
 
-    # Mount the container filesystem
-    MOUNTPOINT=$(buildah mount $CONTAINER)
-
+    # Install the DHCP server package and then remove any cached files
     buildah run $CONTAINER dnf install -y --releasever 41  --nodocs --setopt install_weak_deps=False dhcp-server
     buildah run $CONTAINER dnf clean all -y --releasever 41
 
-    # Cleanup
-    buildah unmount $CONTAINER
-
     # add a volume to include the configuration file
-    buildah config --volume /etc/dhcp $CONTAINER
-    buildah config --volume /var/lib/dhcpd $CONTAINER
-    # add a volume for logs
+    # Leave the files in the default locations 
+    buildah config --volume /etc/dhcp/dhcpd.conf $CONTAINER
+    buildah config --volume /var/lib/dhcpd/dhcp.leases $CONTAINER
 
     # open ports for listening
     buildah config --port 68/udp --port 69/udp ${CONTAINER}
 
+    # Define the startup command
     buildah config --cmd "/usr/sbin/dhcpd -d -cf /etc/dhcp/dhcpd.conf --no-pid" $CONTAINER
 
     # Save the container to an image
     buildah commit --squash $CONTAINER dhcpd-fedora
-
-    #buildah tag localhost/dhcpd-fedora quay.io/markllama/dhcpd-fedora
-
-    #buildah push quay.io/markllama/dhcpd-fedora
 }
 
 
