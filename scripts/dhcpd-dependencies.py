e4cd733adcee901911c2302a1914d87b6775a80e
Mark Lamourine <markllama@gmail.com>
2025-02-24 13:49:56 -0500

    added library resolution

--- scripts/dhcpd-dependencies.py
+++ scripts/dhcpd-dependencies.py
@@ -3,17 +3,29 @@
 Find the binaries in a specified RPM and list the required packages
 and shared libraries needed for each file.
 """
+
+import argparse
+import yaml
+import os
 import subprocess
 import stat
 import sys
-import os
 
 package_dir = "./packages"
 package_name = "dhcp-server"
 
 unpack_dir = "./unpack"
 
-def get_packages(destdir, package):
+def parse_args():
+
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument("--pull", type=bool, default=False)
+    parser.add_argument("--unpack", type=bool, default=False)
+    
+    return parser.parse_args()
+
+def pull_packages(destdir, package):
     """
     Retrieve an RPM and the dependencies from the default repository
     place the RPMs in the directory indicated.
@@ -71,16 +83,48 @@ def find_executable(root_dir):
     # normalize to the OS path (remove the local root dir path
     return [ path.replace(root_dir,'') for path in executables ]
 
-#def find_shared_libraries(root_dir, exe_path):
-    
+def find_shared_libraries(root_dir, exe_path):
+    """
+    Given the root of a tree containing a dynamically linked executable,
+    And the normalized path of the executable file,
+    Get the list of shared libraries.
+    """
+
+    dlink_command = f"ldd { root_dir }{ exe_path}"
+    response = subprocess.check_output(['ldd', f"{ root_dir }{ exe_path }"]).decode('utf-8').split("\n")
+
+    # remove leading tabs
+    lines = [line.strip().split() for line in response]
+    libs = [lib[0] for lib in lines if len(lib) > 0]
+
+    return libs
 
 if __name__ == "__main__":
 
-    packages = get_packages(package_dir, package_name)
+    steps = ['pull', 'unpack', 'exec', 'libs', 'paths', 'pkgs']
+        
+    opts = parse_args()
 
-    for package in packages:
-        unpack_package(os.path.join(package_dir, package), unpack_dir)
+    # * pull
+    # * unpack
     
-    binaries = find_executable(unpack_dir)
+    if opts.pull:
+        packages = pull_packages(package_dir, package_name)
+    else:
+        packages = os.listdir(package_dir)
 
-    print(binaries)
+    if opts.unpack:
+        for package in packages:
+            unpack_package(os.path.join(package_dir, package), unpack_dir)
+    
+    executables = find_executable(unpack_dir)
+
+    print(executables)
+
+    # Find the shared libraries required by the binary
+    shared_libraries = {}
+    for exe_name in executables:
+        shared_libraries[exe_name] = {'libfiles': find_shared_libraries(unpack_dir, exe_name)}
+
+    # Find the path of each shared library
+    print(yaml.dump(shared_libraries, indent=2))
