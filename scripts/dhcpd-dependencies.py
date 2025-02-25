f0a16a2a4cda7b603e71436a521adf2d4eed4733
Mark Lamourine <markllama@gmail.com>
2025-02-24 22:29:23 -0500

    list packages for each library file

--- scripts/dhcpd-dependencies.py
+++ scripts/dhcpd-dependencies.py
@@ -5,11 +5,13 @@ and shared libraries needed for each file.
 """
 
 import argparse
-import yaml
+import distro
 import os
+import re
 import subprocess
 import stat
 import sys
+import yaml
 
 package_dir = "./packages"
 package_name = "dhcp-server"
@@ -24,6 +26,24 @@ def parse_args():
     parser.add_argument("--unpack", type=bool, default=False)
     
     return parser.parse_args()
+
+#
+# Utility functions
+#
+def releasever():
+    response = subprocess.check_output(['dnf', '--dump-variables']).decode('utf-8').split("\n")
+
+    relver = { }
+    
+    for line in response:
+        m = re.match(r"^(\S+) = (\S+)", line)
+        if m is not None:
+            relver[m.group(1)] = m.group(2)
+
+    return relver
+#
+#
+#
 
 def pull_packages(destdir, package):
     """
@@ -83,7 +103,7 @@ def find_executable(root_dir):
     # normalize to the OS path (remove the local root dir path
     return [ path.replace(root_dir,'') for path in executables ]
 
-def find_shared_libraries(root_dir, exe_path):
+def resolve_shared_libraries(root_dir, exe_path):
     """
     Given the root of a tree containing a dynamically linked executable,
     And the normalized path of the executable file,
@@ -95,16 +115,50 @@ def find_shared_libraries(root_dir, exe_path):
 
     # remove leading tabs
     lines = [line.strip().split() for line in response]
-    libs = [lib[0] for lib in lines if len(lib) > 0]
+    libs = [lib[2] for lib in lines if len(lib) > 2]
 
     return libs
+
+def find_lib_package(libname):
+
+    provides_command=f"dnf --quiet provides {libname}"
+
+    response = subprocess.check_output(provides_command.split()).decode('utf-8').split("\n")
+    packages = []
+    #
+    # <pkgname>    : <description>
+    # Repo         : <reponame>
+    # Matched From : blank
+    # Filename     : <filename>
+    # <blank>
+    pkgname = None
+    pkg = {}
+    for line in response:
+        match = re.match(r"^(\S+\s?\S+)\s+:\s+(.*)$", line)
+        if match != None:
+            # figure out which
+            (key, value) = match.groups(1)
+            key = key.lower()
+            if key in ['repo', 'filename']:
+                pkg[key] = value
+            elif key == 'matched from':
+                next
+            else:
+                pkg['name'] = key
+                pkg['description'] = value
+        else:
+            if len(pkg) != 0:
+                packages.append(pkg)
+                pkg = {}
+    
+    return packages
 
 if __name__ == "__main__":
 
     steps = ['pull', 'unpack', 'exec', 'libs', 'paths', 'pkgs']
         
     opts = parse_args()
-
+    
     # * pull
     # * unpack
     
@@ -119,12 +173,36 @@ if __name__ == "__main__":
     
     executables = find_executable(unpack_dir)
 
-    print(executables)
+    # print(executables)
 
     # Find the shared libraries required by the binary
     shared_libraries = {}
     for exe_name in executables:
-        shared_libraries[exe_name] = {'libfiles': find_shared_libraries(unpack_dir, exe_name)}
+        shared_libraries[exe_name] = {
+            'libfiles': resolve_shared_libraries(unpack_dir, exe_name),
+            'packages': {}
+        }
 
     # Find the path of each shared library
-    print(yaml.dump(shared_libraries, indent=2))
+    # print(yaml.dump(shared_libraries, indent=2))
+
+    for exe in shared_libraries.keys():
+        # Find the package that provides that file
+        for lib in shared_libraries[exe]['libfiles']:
+            try:
+                path = '/usr' + lib
+                pkg = find_lib_package(path)
+            except subprocess.CalledProcessError:
+                path = lib
+                pkg = find_lib_package(path)
+
+            shared_libraries[exe]['packages'][path] = pkg
+
+        for libfile in shared_libraries[exe]['packages'].keys():
+            
+            # now find the hightest version package
+            versions = [ x['name'] for x in shared_libraries[exe]['packages'][libfile] ]
+            versions.sort(reverse=True)
+            print(f"Library File: { libfile } from package {versions[0]}")
+
+    
