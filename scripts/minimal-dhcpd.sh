c6dfc30e71a9bbb2833751b9b6a21b4c469392fd
Mark Lamourine <markllama@gmail.com>
2025-02-28 19:39:32 -0500

    add skeleton script to build minimal DHCP container

--- scripts/minimal-dhcpd.sh
+++ scripts/minimal-dhcpd.sh
@@ -13,31 +13,34 @@ set -o errexit
 OPT_SPEC='n:r:'
 
 DEFAULT_NAME="dhcpd"
-DEFAULT_ROOT="${pwd}/root"
+DEFAULT_ROOT="minimal/model"
 
 : NAME="${NAME:=${DEFAULT_NAME}}"
 : ROOT="${ROOT:=${DEFAULT_ROOT}}"
 
 function main() {
 
+    parse_args
+    
     # Create a container
     CONTAINER=$(buildah from --name ${NAME} scratch)
 
     # copy file tree
+    buildah copy ${CONTAINER} --contextdir ${ROOT}
     
     # add a volume to include the configuration file
     # Leave the files in the default locations 
-    # buildah config --volume /etc/dhcp/dhcpd.conf $CONTAINER
-    # buildah config --volume /var/lib/dhcpd/dhcp.leases $CONTAINER
+    buildah config --volume /etc/dhcp/dhcpd.conf $CONTAINER
+    buildah config --volume /var/lib/dhcpd/dhcp.leases $CONTAINER
 
     # # open ports for listening
-    # buildah config --port 68/udp --port 69/udp ${CONTAINER}
+    buildah config --port 68/udp --port 69/udp ${CONTAINER}
 
     # # Define the startup command
-    # buildah config --cmd "/usr/sbin/dhcpd -d --no-pid" $CONTAINER
+    buildah config --cmd "/usr/sbin/dhcpd -d --no-pid" $CONTAINER
 
     # # Save the container to an image
-    # buildah commit --squash $CONTAINER
+    buildah commit --squash $CONTAINER
 }
 
 function parse_args() {
